345a00407fab19352abf19f3d94559c10d32c4d5
jdvr <juandavidvegarguez@gmail.com>
2019-09-11 09:37:42 +0200

    improvements

--- src/main/java/jdvr/ForthEvaluator.java
+++ src/main/java/jdvr/ForthEvaluator.java
@@ -1,6 +1,5 @@
 package jdvr;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -11,13 +10,14 @@ import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ForthEvaluator  {
+public class ForthEvaluator {
 
 	public final static Map<TokenType, BiFunction<Integer, Integer, List<Integer>>> TwoValueOperationByToken = Map.of(
 			TokenType.ADD, (left, right) -> List.of(left + right),
 			TokenType.SUBTRACT, (left, right) -> List.of(left - right),
 			TokenType.MULTIPLICATION, (left, right) -> List.of(left * right),
 			TokenType.SWAP, (left, right) -> List.of(right, left),
+			TokenType.OVER, (first, second) -> List.of(first, second, first),
 			TokenType.DIVISION, (left, right) -> {
 				if (right == 0) {
 					throw new IllegalArgumentException("Division by 0 is not allowed");
@@ -29,11 +29,6 @@ public class ForthEvaluator  {
 	public final static Map<TokenType, IntFunction<List<Integer>>> SingleValueOperationByToken = Map.of(
 			TokenType.DUP, value -> List.of(value, value),
 			TokenType.DROP, value -> Collections.emptyList()
-	);
-
-	public final static Map<TokenType, ThreeValueIntFunction> ThreeValuesOperationByToken = Map.of(
-			// I think the test for this command is wrong
-			TokenType.OVER, (first, second, third) -> List.of(first, second, first)
 	);
 
 	private final Map<String, VariableToken> variables;
@@ -62,77 +57,68 @@ public class ForthEvaluator  {
 			for (var t : variableValue) {
 				evaluate(down, t);
 			}
-		} else {
-			if (TwoValueOperationByToken.containsKey(token.type())) {
-				if (down.isEmpty() || down.size() < 2) {
-					throw new IllegalArgumentException(token.type().toString() + " requires that the stack contain at least 2 values");
-				}
-				var right = down.pop();
-				var left = down.pop();
-				var res = TwoValueOperationByToken.get(token.type()).apply(left, right);
-				down.addAll(res);
-			} else if (ThreeValuesOperationByToken.containsKey(token.type())) {
-				if (down.isEmpty() || down.size() < 2) {
-					throw new IllegalArgumentException(token.type().toString() + " requires that the stack contain at least 2 values");
-				}
-				var right = down.pop();
-				var left = down.pop();
-				var res = ThreeValuesOperationByToken.get(token.type()).apply(left, right, down.size());
-				down.addAll(res);
-			} else {
-				if (down.isEmpty()) {
-					throw new IllegalArgumentException(token.type().toString() + " requires that the stack contain at least 1 value");
-				}
-				var value = down.pop();
-				var res = SingleValueOperationByToken.get(token.type()).apply(value);
-				down.addAll(res);
+		} else if (TwoValueOperationByToken.containsKey(token.type())) {
+			if (down.isEmpty() || down.size() < 2) {
+				throw new IllegalArgumentException(token.type().toString() + " requires that the stack contain at least 2 values");
 			}
-
+			var right = down.pop();
+			var left = down.pop();
+			var res = TwoValueOperationByToken.get(token.type()).apply(left, right);
+			down.addAll(res);
+		} else {
+			if (down.isEmpty()) {
+				throw new IllegalArgumentException(token.type().toString() + " requires that the stack contain at least 1 value");
+			}
+			var value = down.pop();
+			var res = SingleValueOperationByToken.get(token.type()).apply(value);
+			down.addAll(res);
 		}
 	}
 
 	private List<Token> parse(String line) {
-		if (line.startsWith(": ")) {
-			var withoutColonAndSemiColon = line
-					.replace(":", "")
-					.replace(";", "")
-					.trim();
-			var parts = withoutColonAndSemiColon.split(" ");
-			var variableName = parts[0];
-			if (variableName.matches("[\\d]+"))
-				throw new IllegalArgumentException("Cannot redefine numbers");
-			var tokens = new ArrayList<Token>();
-			for (int i = 1; i < parts.length; i++) {
-				tokens.add(parseSingleWord(parts[i]));
-			}
-			variables.put(variableName.toLowerCase(), new VariableToken(tokens));
-			return List.of();
+		boolean isNotVariableDefinition = !line.startsWith(": ");
+		if (isNotVariableDefinition) {
+			return Stream.of(line.split("\\s"))
+					.map(this::parseSingleWord)
+					.collect(Collectors.toList());
 		}
-		return Stream.of(line.split("\\s"))
-				.map(this::parseSingleWord)
-				.collect(Collectors.toList());
+		String withoutColonAndSemiColon = removeColonAndSemicolon(line);
+		String variableName = getVariableName(withoutColonAndSemiColon).toLowerCase();
+		var parts = List.of(withoutColonAndSemiColon.split("\\s"));
+		var rawValues = parts.subList(1, parts.size());
+		var tokens = rawValues.stream().map(this::parseSingleWord).collect(Collectors.toList());
+		variables.put(variableName, new VariableToken(tokens));
+		return List.of();
 	}
 
-	private Token parseSingleWord(String s) {
-		if (s.matches("[\\d]+")){
-			return new NumberToken(Integer.valueOf(s));
+	private String removeColonAndSemicolon(String line) {
+		return line
+				.replace(":", "")
+				.replace(";", "")
+				.trim();
+	}
+
+	private String getVariableName(String withoutColonAndSemiColon) {
+		var variableName = withoutColonAndSemiColon.substring(0, withoutColonAndSemiColon.indexOf(" "));
+		if (variableName.matches("[\\d]+")) {
+			throw new IllegalArgumentException("Cannot redefine numbers");
+		}
+		return variableName;
+	}
+
+	private Token parseSingleWord(String singleWord) {
+		var lowerCaseInput = singleWord.toLowerCase();
+		if (lowerCaseInput.matches("[\\d]+")) {
+			return new NumberToken(Integer.valueOf(singleWord));
+		} else if (variables.containsKey(lowerCaseInput)) {
+			return variables.get(lowerCaseInput);
 		} else {
-			var lowerCaseInput = s.toLowerCase();
-			if (variables.containsKey(lowerCaseInput)) {
-				return variables.get(lowerCaseInput);
-			}
 			return Stream.of(TokenType.values())
 					.filter(tokenType -> tokenType.match(lowerCaseInput))
 					.findFirst()
 					.map(tt -> (Token) new OperationToken(tt))
-					.orElseThrow(() -> new IllegalArgumentException(String.format("No definition available for operator \"%s\"", s)));
+					.orElseThrow(() -> new IllegalArgumentException(String.format("No definition available for operator \"%s\"", singleWord)));
 		}
 	}
-
-	@FunctionalInterface
-	interface ThreeValueIntFunction {
-		List<Integer> apply (int first, int second, int third);
-	}
-
 
 }
